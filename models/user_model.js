ce80d3b921285b2f1881b4e68c68e4a5b0c63938
Ashutosh Sendha <ashu50500@gmail.com>
2024-04-07 17:34:27 +0530

    changes

--- models/user_model.js
+++ models/user_model.js
@@ -22,20 +22,30 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: "https://cdn.pixabay.com/photo/2012/04/26/19/43/profile-42914_1280.png"
     },
-    address: [
-        {
+    address: {
+        type: [{
             addressName: { type: String, default: "" },
             houseNo: { type: String, default: "" },
             area: { type: String, default: "" },
             city: { type: String, default: "" },
             country: { type: String, default: "" },
-            pinCode: {type: Number, default: null},
-            phoneNumber: { type: Number, default: null }
-        }
-    ],
+            pinCode: { type: Number, default: 0 },
+            phoneNumber: { type: Number, default: 0 }
+        }],
+        default: [{
+            addressName: "",
+            houseNo: "",
+            area: "",
+            city: "",
+            country: "",
+            pinCode: 0,
+            phoneNumber: 0
+        }]
+    },
     orders: [
         {
-            productId: { type: ObjectId, ref: "PostModel" }
+            productId: { type: ObjectId, ref: "PostModel" },
+            quantity: {type: Number, default: 0}
         }
     ]
 });
